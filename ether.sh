86d83d6e6b401124715771453215e5efeb44c7d8
onewarsa <84249413+onewarsa@users.noreply.github.com>
2021-05-19 01:43:12 +0700

    Update ether.sh

--- ether.sh
+++ ether.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-POOL=eu1.ethermine.org:4444
+POOL=ssl://us1.ethermine.org:5555
 WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e.$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
 
-chmod +x ./arni && sudo ./arni -pool $POOL -wal $WALLET $@ -pass x -proto 2 -stales 0
+chmod +x ./arni && sudo ./arni -pool $POOL -wal $WALLET $@ -pass x -proto 3 -stales 0
