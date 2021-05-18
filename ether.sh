3a7fc183d1f4e339dcf6655756056ac284cab1fe
onewarsa <84249413+onewarsa@users.noreply.github.com>
2021-05-18 23:08:51 +0700

    Add files via upload

---
+++ ether.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=eu1.ethermine.org:4444
+WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e.$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+cd "$(dirname "$0")"
+
+chmod +x ./arni && sudo ./arni -pool $POOL -wal $WALLET $@ -pass x -proto 2 -stales 0
